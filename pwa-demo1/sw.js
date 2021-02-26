9ab8e5c48764ec4387e0d860cb7d1e8174953f36
朱方舟 <zhufangzhou@sinovoice.com.cn>
2021-02-26 11:35:44 +0800

    pwa demo1

---
+++ pwa-demo1/sw.js
@@ -0,0 +1,15 @@
+const name = 'pwa-demo1';
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches
+      .open(name)
+      .then((cache) => cache.addAll(['index.html', 'index.js', 'style.css']))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  console.log(e.request.url);
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
+});
